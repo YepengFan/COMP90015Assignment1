878df531a792d93f3d5f09d13f029d4094bc1a7a
Yepeng Fan <fanyepeng@yeah.net>
2015-04-02 09:10:32 +1100

    remove useless comments

--- src/filesync/SyncClient.java
+++ src/filesync/SyncClient.java
@@ -153,36 +153,10 @@ public class SyncClient {
 
     public static void main(String[] args) throws IOException {
         String hostname = args[1];
-//        SynchronisedFile fromFile = null;
-
         socket = new Socket(hostname, serverPort);
         System.out.println("Connection Established");
         in = new DataInputStream(socket.getInputStream());
         out = new DataOutputStream(socket.getOutputStream());
-
-//         try{
-//             s = new Socket(hostname, serverPort);
-//             System.out.println("Connection Established");
-//             DataInputStream in = new DataInputStream(s.getInputStream());
-//             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-//             System.out.println("Sending Data");
-//             out.writeUTF(message);
-//             String data = in.readUTF();
-//             System.out.println("Received: " + data);
-//         }catch (UnknownHostException e){
-//             System.out.println("Socket: " + e.getMessage());
-//         }catch (EOFException e){
-//             System.out.println("EOF: " + e.getMessage());
-//         }catch (IOException e){
-//             System.out.println("Readline: " + e.getMessage());
-//         }finally {
-//             if(s != null) try{
-//                 s.close();
-//             }catch (IOException e){
-//                 System.out.println("close: " + e.getMessage());
-//             }
-//         }
-
         boolean recursive = false;
         Path dir = Paths.get(args[0]);
         new SyncClient(dir, recursive).processEvents();
